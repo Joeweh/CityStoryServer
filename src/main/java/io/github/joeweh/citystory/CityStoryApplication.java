e8180a0591e352dbf3cae1f5ceb19c7e97814fb5
Joeweh <joeyqsa@outlook.com>
2025-04-07 15:42:41 -0400

    Spring project template

---
+++ src/main/java/io/github/joeweh/citystory/CityStoryApplication.java
@@ -0,0 +1,11 @@
+package io.github.joeweh.citystory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityStoryApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CityStoryApplication.class, args);
+  }
+}
---
+++ src/test/java/io/github/joeweh/citystory/CityStoryApplicationTests.java
@@ -0,0 +1,10 @@
+package io.github.joeweh.citystory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityStoryApplicationTests {
+	@Test
+	void contextLoads() {}
+}
