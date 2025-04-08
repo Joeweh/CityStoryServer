3a88794fee482229345570d6df014514074c674a
Joeweh <joeyqsa@outlook.com>
2025-04-08 03:42:34 -0400

    Misc formatting

--- src/main/java/io/github/joeweh/citystory/CityStoryApplication.java
+++ src/main/java/io/github/joeweh/citystory/CityStoryApplication.java
@@ -1,9 +1,10 @@
 package io.github.joeweh.citystory;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"io.github.joeweh.citystory"})
 public class CityStoryApplication {
   public static void main(String[] args) {
     SpringApplication.run(CityStoryApplication.class, args);
